5324e771204f108a060582aacf3cb13d4561ebf3
riatitova <mary.titova.1995@gmail.com>
2020-09-14 17:26:21 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let stack = [];
+ 
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (stack.length == 0 && str[i] == bracketsConfig[j][0]) {
+        stack.push(str[i]);
+      } else if (stack.length != 0) {
+        //if close bracket
+        if (str[i] == bracketsConfig[j][1]) {        
+          if (stack[stack.length - 1] == bracketsConfig[j][0]) {          
+            stack.pop();
+          } else if (str[i] == bracketsConfig[j][1] && bracketsConfig[j][0] != bracketsConfig[j][1]) {
+            return false;
+          } else if (bracketsConfig[j][0] == bracketsConfig[j][1]){
+            if (stack.includes(str[i])) {
+              return false; 
+            }else {
+              stack.push(str[i]);
+            }
+
+          }
+          //if open bracket
+        } else if (str[i] == bracketsConfig[j][0]) {
+            stack.push(str[i]);
+          }  
+        } else if (stack.length == 0 && str[i] == bracketsConfig[j][1]){
+          return false;
+        }
+
+      }
+    } 
+  if (stack.length != 0) {
+    return false;
+  }
+
+  return true;
 }
